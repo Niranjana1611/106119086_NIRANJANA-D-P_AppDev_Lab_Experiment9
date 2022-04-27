5e71d55125e1f687624860ca48ae43613fb7428d
Niranjana1611 <niranjanadhanasekaran@gmail.com>
2022-04-27 18:10:33 +0530

    106119086_NIRANJANA-D-P_AppDev_Lab_Experiment9

---
+++ app/src/main/java/com/example/week7_qn9/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.week7_qn9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
